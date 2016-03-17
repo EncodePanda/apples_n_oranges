8c95fc2ab556afb76679b64ad2bde09abcbba37a
Paweł Szulc <paul.szulc@gmail.com>
2016-03-17 22:06:25 +0100

    Fix imports

--- src/test/scala/ano/CheckoutTest.scala
+++ src/test/scala/ano/CheckoutTest.scala
@@ -1,15 +1,12 @@
 package ano
 
 import org.scalatest._
-import org.scalatest.Matchers._
-import org.scalatest.prop.GeneratorDrivenPropertyChecks
-import org.scalacheck.Gen
 
 class CheckoutTest extends FunSpec with Matchers {
 
   import Checkout._
 
-  describe("Checkout") {
+  describe("Checkout.totalCost") {
     it("should return zero for empty list") {
       totalCost(Seq.empty[Item]) should equal(BigDecimal("0"))
     }
